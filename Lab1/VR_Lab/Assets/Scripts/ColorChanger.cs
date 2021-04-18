8b461b950898bea26a11b109c271a6ffc51c4b23
Sevcan ALKAN <sevcanalkan@outlook.com.tr>
2021-04-19 01:15:14 +0300

    Lab1 and Lab2 completed

---
+++ Lab1/VR_Lab/Assets/Scripts/ColorChanger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChanger : MonoBehaviour
+{
+    public Color newColor;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.gameObject.name);
+
+        if (collision.gameObject.tag.Equals("ColorChanger"))
+        {
+            GetComponent<Renderer>().material.color = newColor;    
+        }
+    }
+}
